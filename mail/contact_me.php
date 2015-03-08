023ccd3a96df1cf1e44ca8aaa26fe5239a1b3a9e
Ivan Teong <ivanteong@hotmail.com>
2015-03-08 14:28:54 +1100

    Email code

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -40,9 +40,7 @@ if(empty($_POST['name'])  		||
        Email: $email\n
        Subject: $phone\n
        Message: $message",
-      'from' => 'noreply@ivanteong.com', // set from address here, it can really be anything
-      mail($to,$subject,$html,$text,$from);
-      return true;      
+      'from' => 'noreply@ivanteong.com', // set from address here, it can really be anything     
    );
 
  curl_setopt($curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
